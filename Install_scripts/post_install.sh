14f159a01a49c439845b635eb13afb53b4741249
Bastinator-school <145536512+Bastinator-school@users.noreply.github.com>
2024-12-25 20:38:05 +0100

    added preseed.cfg and cleaned up a bit

--- Install_scripts/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub com.valvesoftware.Steam
-flatpak install flathub com.github.iwalton3.jellyfin-media-player
-flatpak install flathub com.discordapp.Discord
